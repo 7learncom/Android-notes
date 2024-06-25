41a62f459646ec12201b3a1e9638352424a1a534
MHD <m.dabbaghy@gmail.com>
2024-06-25 13:26:37 +0330

    Add navigation

---
+++ app/src/main/java/com/mhd/noteapp/add/AddNoteFragment.kt
@@ -0,0 +1,7 @@
+package com.mhd.noteapp.add
+
+import androidx.fragment.app.Fragment
+import com.mhd.noteapp.R
+
+class AddNoteFragment: Fragment(R.layout.fragment_add) {
+}
---
+++ app/src/main/java/com/mhd/noteapp/list/NoteListFragment.kt
@@ -0,0 +1,7 @@
+package com.mhd.noteapp.list
+
+import androidx.fragment.app.Fragment
+import com.mhd.noteapp.R
+
+class NoteListFragment: Fragment(R.layout.fragment_list) {
+}
